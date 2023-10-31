9f37e519d599aa0b0a542e28b19916bd15a72746
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-10-30 18:58:59 -0600

    w1d1 coding exercises

---
+++ week_1/day_1/w1d1_practice.py
@@ -0,0 +1,24 @@
+# ITP Week 1 Day 1 (In-Class) Practice
+
+# First run this file to see what the expected output is for the following questions.
+
+print('      /|')
+print('     / |')
+print('    /  |')
+print('   /___|\n')
+
+# recreate this triangle with a height of 6
+
+print('        /|')
+print('       / |')
+print('      /  |')
+print('     /   |')
+print('    /    |')
+print('   /_____|\n')
+
+# recreate the 4 level triangle with a single line using \n
+
+print('      /|\n     / |\n    /  |\n   /___|\n')
+
+
+# recreate the 4 level triangle using a multi-line string

1f4368aac02ba87ec16e5663e3d589dc3c5279c2
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-11-07 17:46:42 -0700

    created explict path and prog worked xlsx created

--- week_2/day_1/practice.py
+++ week_2/day_1/practice.py
@@ -11,7 +11,7 @@ print(type(wb))
 ws = wb.active
 
 # A4. assign the value of "First Name" to A1
-first_cell = ws['A1'].value = "First Name"
+first_cell = ws['A1'] = "First Name"
 
 # A5. assign the value of "Last Name" to B1
 next_cell = ws['B1'] = "Last Name"
@@ -20,20 +20,21 @@ next_cell = ws['B1'] = "Last Name"
 
 # B1. For all of column A, starting at row 2 until row 10, make the cell values: "Gabriel" (attempt a loop)
 
-for sheet in range(2, 11):
-    pass
+# for sheet in range(2, 11):
+#     pass
 
-column_a = ws['A']
-for cell in column_a:
-    cell.value = 'Gabriel'
+# column_a = ws['A']
+# for cell in column_a:
+#     cell.value = 'Gabriel'
 
-last_names = ['Rolley', 'Smith', 'Balenga', 'Issac', 'Cruise', 'Depp', 'Heard', 'Qiao', 'Biden']
+# last_names = ['Rolley', 'Smith', 'Balenga', 'Issac', 'Cruise', 'Depp', 'Heard', 'Qiao', 'Biden']
 
-# B2. Loop through a range from row 2 to 10 and assign the cell value to last names according to index in column B
-# NOTE: PAY ATTENTION to the starting number of the range and how it differs from the starting index of the list
+# # B2. Loop through a range from row 2 to 10 and assign the cell value to last names according to index in column B
+# # NOTE: PAY ATTENTION to the starting number of the range and how it differs from the starting index of the list
 
-for row in ws.iter_rows(min_row=2, max_row=10, min_col=2): # rows 2-10, column B
-    for name in last_names:
-        cell.value = name
-# B3. Save the file
-wb.save("./spreadsheets/day_1_practice.xlsx")
+# for row in ws.iter_rows(min_row=2, max_row=10, min_col=2): # rows 2-10, column B
+#     for name in last_names:
+#         cell.value = name
+# # B3. Save the file
+# wb.save("./spreadsheets/day_1_practice.xlsx")
+wb.save("/home/ra11y/Downloads/local.source/vetsInTech/week_2/spreadsheets/day_1_practice.xlsx")

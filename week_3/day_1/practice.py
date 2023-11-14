c51f8c71dc9b7edbf604868235086e3a48d57b17
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-11-13 20:38:21 -0700

    w3d1 practice

--- week_3/day_1/practice.py
+++ week_3/day_1/practice.py
@@ -7,7 +7,7 @@ wb = Workbook()
 ws = wb.active
 
 # use an external counter with just a for loop (no function)
-counter = 0 # initialize the counter
+
 clefairy = {
     "id": 35,
     "name": "clefairy",
@@ -16,7 +16,14 @@ clefairy = {
     "order": 56,
     "weight": 75,
 }
-
+counter = 0 # initialize the counter
+for num in range(1, len(clefairy)):
+    ws.cell(row=1, column=num , value=clefairy['id'])
+    ws.cell(row=1, column=num , value=clefairy['name'])
+    ws.cell(row=1, column=num , value=clefairy['base_experience'])
+    ws.cell(row=1, column=num , value=clefairy['height'])
+    ws.cell(row=1, column=num , value=clefairy['order'])
+    ws.cell(row=1, column=num , value=clefairy['weight'])
 
 # create a function that takes in a pokemon
 weedle = {
@@ -30,7 +37,11 @@ weedle = {
 
 # call the function with weedle!
 def pokemon_fcn(weedle):
+    ws.cell(row=1, column=1, value=weedle['id'])
+    ws.cell(row=1, column=2, value=weedle['name'])
+    pass
 
+#
 # wb.save('./spreadsheets/practice.xlsx')
 # absolute path
-wb.save('/home/ra11y/Downloads/local.source/vetsInTech/week_3/spreadsheets/practice.xlsx')
+wb.save('/home/ra11y/Downloads/local.source/vetsInTech/week_3/spreadsheets/w3_d1_practice.xlsx')

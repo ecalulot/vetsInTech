1cb30c9b9deba838483a6cdb42b6ee150e6cc1b7
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-11-15 19:02:44 -0700

    w3d3 practice

--- week_3/day_3/practice.py
+++ week_3/day_3/practice.py
@@ -1,7 +1,7 @@
 # ITP Week 3 Day 3 Practice
 
 # import in the two modules for making API calls and parsing the data
-import requests
+import requests, json
 
 # Using our API call from the exercise yesterday (this is provided for you):
 
@@ -9,35 +9,37 @@ import requests
 url = "https://rickandmortyapi.com/api/character"
 response = requests.get(url)
 json_data = response.text
-# data = json.loads(json_data)
+data = json.loads(json_data)
 
 # print it out to see how the data looks initially:
-
+print(type(data))
+print(data)
 # Convert the very hard to read data back into JSON using json.dumps (store into a serialized_json variable):
-
+serialized_json = json.dumps({"???test":"???good"}) # see lecture notes
 # print the serialized_json
-
+print(type(serialized_json))
+print(serialized_json)
 # This doesn't look too different than our original print at a glance
 
 # Add some appropriate indents to make it more readable reassigning serialized_json:
 
 # uncomment below to see the changes!
-# print(serialized_json)
+print(serialized_json)
 
 # This makes it look a lot more readable
 
 # Now change all of the separators from (,  :)  into (:  =>) reassigning serialized_json
 
 # uncomment below to see the changes!
-# print(serialized_json)
+print(serialized_json)
 
 # This one really has no purpose for this content,
 # it can just give us a better looking separation of values.
 
 # Finally, sort the data alphabetically reassigning serialized_json:
-
+sort_json = sorted(serialized_json)
 # uncomment below to see the changes!
-# print(serialized_json)
+print(serialized_json)
 
 # Again, this one is just for you the programmer to be able to easily
 # sort through the data that you have gotten from the api alphabetically.
